16295325cda88c645a2a1ae1892ef1e413673566
Livia Devolder <liviadevolder@gmail.com>
2022-11-09 13:35:54 -0300

    feat: add roulete structure

---
+++ pages/roleta.tsx
@@ -0,0 +1,5 @@
+export default function Roleta() {
+  return (
+    <p>text</p>
+  );
+}
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,16 +1,39 @@
 import { Flex, Text } from "@chakra-ui/react";
+import Link from "next/link";
 import Lemon from "../../../public/Lemon";
 
 export default function Menu() {
   return (
-    <Flex w={'100%'} justifyContent="center" gap={'52px'} alignItems={'center'} lineHeight="56px" pb={'20px'} borderBottom="2px solid #310E2B">
-      <Text fontFamily={"Raleway"} fontSize={"40px"} color={"#310E2B"}>
-        Receitas
-      </Text>
+    <Flex
+      w={"100%"}
+      justifyContent="center"
+      gap={"52px"}
+      alignItems={"center"}
+      lineHeight="56px"
+      pb={"20px"}
+      borderBottom="2px solid #310E2B"
+    >
+      <Link href="/">
+        <Text
+          cursor={"pointer"}
+          fontFamily={"Raleway"}
+          fontSize={"40px"}
+          color={"#310E2B"}
+        >
+          Receitas
+        </Text>
+      </Link>
       <Lemon />
-      <Text fontFamily={"Raleway"} fontSize={"40px"} color={"#310E2B"}>
-        Roleta
-      </Text>
+      <Link href="/roleta">
+        <Text
+          cursor={"pointer"}
+          fontFamily={"Raleway"}
+          fontSize={"40px"}
+          color={"#310E2B"}
+        >
+          Roleta
+        </Text>
+      </Link>
     </Flex>
   );
 }
